e792663e0a412f5d0d0640178427d773ee52ee18
runic-tears <51153795+runic-tears@users.noreply.github.com>
2019-08-19 22:21:47 +0400

    Больше доброты-милоты

--- Twig/Utils/HugMessages.py
+++ Twig/Utils/HugMessages.py
@@ -1,14 +1,17 @@
 import random
 
-HugMessages = [
-    'Хэй, %%target%%! Тебя только-что обнял **%%sender%%**!',
-    'Ой, как это мило! **%%sender%%** обнял %%target%% :heart:',
-    'Ой-ой, %%target%%, кажется... Тебя только что обнял **%%sender%%**!',
-]
-
 
 def do_hug(target, sender):
     chosen_msg = random.choice(HugMessages)
     chosen_msg = chosen_msg.replace('%%target%%', str(target))
     chosen_msg = chosen_msg.replace('%%sender%%', str(sender))
     return chosen_msg
+
+
+HugMessages = [
+    'Хэй, %%target%%! Тебя только-что обнял **%%sender%%**! **(｡>﹏<｡)**',
+    'Ой, как это мило! **%%sender%%** обнял %%target%% :heart:',
+    'Ой-ой, %%target%%, кажется... Тебя только что обнял **%%sender%%**! **(≧◡≦)**',
+    '***%%sender%%*** *обнимает %%target%%*',
+    '**%%sender%%** устроаивает время обнимашек! %%target%%, иди, обниму **(っ˘ω˘ς )**',
+]
