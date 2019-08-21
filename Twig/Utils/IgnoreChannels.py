3b9bda7d1771566cb4cdfea26b2b4c26f6daad1d
runic-tears <51153795+runic-tears@users.noreply.github.com>
2019-08-21 10:35:40 +0400

    Update IgnoreChannels.py

--- Twig/Utils/IgnoreChannels.py
+++ Twig/Utils/IgnoreChannels.py
@@ -1,5 +1,5 @@
 LEVELS_IGNORED_CHANNELS = [
     "commands", "bot-spam", "bots", "команды", "боты", "флуд", "flood", "spam",
     "спам", "announcements", "анонсы", "новости", "объявления", "keepers-general",
-    "keepers-commands"
+    "keepers-commands", "play-this"
 ]
