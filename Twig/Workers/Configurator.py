ae512848193c8a6742bcdd4c3bca85143eeb781e
runic-tears <51153795+runic-tears@users.noreply.github.com>
2019-08-22 19:19:30 +0400

    Аптемезацыя

--- Twig/Workers/Configurator.py
+++ Twig/Workers/Configurator.py
@@ -12,3 +12,5 @@ BOT_PLAYING_GAME_NAME = config['STATUS']
 BOT_STARTED_AT = time.time()
 start_time = BOT_STARTED_AT
 is_no_perm_notifications_enabled = config['SHOW_NO_PERMS_MESSAGES']
+
+f.close()
